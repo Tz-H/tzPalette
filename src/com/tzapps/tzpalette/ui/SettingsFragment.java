2519feee3e089afaf7b96f90954e081c76655d4e
tianzi.hou <tianzi.hou@gmail.com>
2013-09-30 20:24:39 +0100

    works: 1. introduce the NumberPickerPreference and SeekBarDialogPreference 2. add NumberPickerPreference into settings view

--- src/com/tzapps/tzpalette/ui/SettingsFragment.java
+++ src/com/tzapps/tzpalette/ui/SettingsFragment.java
@@ -2,11 +2,20 @@ package com.tzapps.tzpalette.ui;
 
 import com.tzapps.tzpalette.R;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.util.Log;
 
-public class SettingsFragment extends PreferenceFragment
+public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener
 {
+    private static final String TAG = "SettingsFragment";
+    
+    public static final String KEY_PREF_COLOR_TYPE = "pref_analysisColorType";
+    public static final String KEY_PREF_COLOR_NUMBER = "pref_setColorNumber";
+    
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -16,4 +25,36 @@ public class SettingsFragment extends PreferenceFragment
         addPreferencesFromResource(R.xml.preferences);
     }
 
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+    {
+        if (key.equals(KEY_PREF_COLOR_TYPE))
+        {
+            Preference colorTypePref = findPreference(key);
+            String defaultColorType = getResources().getString(R.string.pref_analysisColorType_default);
+            String colorType = sharedPreferences.getString(key, defaultColorType);
+            colorTypePref.setSummary(colorType);
+        }
+        else if (key.equals(KEY_PREF_COLOR_NUMBER))
+        {
+            Preference colorNumberPref = findPreference(key);
+            int defaultColorNumber = getResources().getInteger(R.integer.pref_setColorNumber_default);
+            int colorNumber = sharedPreferences.getInt(key, defaultColorNumber);
+            colorNumberPref.setSummary(colorNumber + "");
+        }
+    }
+    
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+    
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
 }
